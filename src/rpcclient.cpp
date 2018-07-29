2dfdfb1825111d0db8e1253d776bac3b33434421
wcAlex <alex.chi.wang@gmail.com>
2018-07-28 23:13:40 -0400

    fix setgenerate parameter validation

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -154,6 +154,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "checkkernel", 0 },
     { "checkkernel", 1 },
     { "submitblock", 1 },
+    { "setgenerate", 0 },
+    { "setgenerate", 1 },
 };
 
 class CRPCConvertTable
